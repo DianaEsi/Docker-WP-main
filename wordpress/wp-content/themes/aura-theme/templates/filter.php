0c1204c873e4484caac5ab2839cc123d88be9d35
DianaEsi <esieva.dia@mail.ru>
2026-04-27 16:00:03 +0300

    Доработала стили фильтра

--- wordpress/wp-content/themes/aura-theme/templates/filter.php
+++ wordpress/wp-content/themes/aura-theme/templates/filter.php
@@ -1,11 +1,9 @@
-
 <section class="catalog-filter">
     <div class="filter">
-        <div class="filter__mobile"> <img src="<?php echo get_template_directory_uri(); ?>/assets/images/page_info.svg" alt="Инфо">
-
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/arrow_forward.svg" alt="Стрелка" style="vertical-align: middle">
+        <div class="filter__mobile">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/page_info.svg" alt="Инфо" style="vertical-align: middle;">
+            <button class="mobile-filter-btn" style="background: none; border: none; font-size: 16px; cursor: pointer;">Фильтры</button>
         </div>
-        <button class="mobile-filter-btn" style="background: none; border: none; font-size: 16px; cursor: pointer;">Фильтры</button>
     </div>
     <div class="filter-left">
 
@@ -14,34 +12,34 @@
                 <button class="filter-btn">
                     Тип квартиры <img src="<?php echo get_template_directory_uri(); ?>/assets/images/keyboard_arrow_down.svg" alt="Стрелка" style="vertical-align: middle">
                 </button>
+                <div class="filter-dropdown">
+                    <label style="margin-bottom: 0px;"><input type="checkbox" style="width: auto; box-shadow: none;"> Студия</label>
+                    <label style="margin-bottom: 0px;"><input type="checkbox" style="width: auto; box-shadow: none;"> Однокомнатная</label>
+                    <label style="margin-bottom: 0px;"><input type="checkbox" style="width: auto; box-shadow: none;"> Двухкомнатная</label>
+                    <label style="margin-bottom: 0px;"><input type="checkbox" style="width: auto; box-shadow: none;"> Трехкомнатная</label>
+                </div>
             </div>
-
-            <div class="filter-dropdown">
-                <label style="margin-bottom: 0px;"><input type="checkbox" style="width: auto; box-shadow: none;"> Студия</label>
-                <label style="margin-bottom: 0px;"><input type="checkbox" style="width: auto; box-shadow: none;"> Однокомнатная</label>
-                <label style="margin-bottom: 0px;"><input type="checkbox" style="width: auto; box-shadow: none;"> Двухкомнатная</label>
-                <label style="margin-bottom: 0px;"><input type="checkbox" style="width: auto; box-shadow: none;"> Трехкомнатная</label>
-            </div>
-
         </div>
+
 
         <div class="filter-item">
             <div class="item">
                 <button class="filter-btn">
                     Кол-во гостей <img src="<?php echo get_template_directory_uri(); ?>/assets/images/keyboard_arrow_down.svg" alt="Стрелка" style="vertical-align: middle">
                 </button>
-                    <div class="filter-dropdown">
-                        <label style="margin-bottom: 0px;"><input type="checkbox" style="width: auto; box-shadow: none;"> 2</label>
-                        <label style="margin-bottom: 0px;"><input type="checkbox" style="width: auto; box-shadow: none;"> 3</label>
-                        <label style="margin-bottom: 0px;"><input type="checkbox" style="width: auto; box-shadow: none;"> 4</label>
-                        <label style="margin-bottom: 0px;"><input type="checkbox" style="width: auto; box-shadow: none;"> 5</label>
-                        <label style="margin-bottom: 0px;"><input type="checkbox" style="width: auto; box-shadow: none;"> 6 и более</label>
-                    </div>
+                <div class="filter-dropdown">
+                    <label style="margin-bottom: 0px;"><input type="checkbox" style="width: auto; box-shadow: none;"> 2</label>
+                    <label style="margin-bottom: 0px;"><input type="checkbox" style="width: auto; box-shadow: none;"> 3</label>
+                    <label style="margin-bottom: 0px;"><input type="checkbox" style="width: auto; box-shadow: none;"> 4</label>
+                    <label style="margin-bottom: 0px;"><input type="checkbox" style="width: auto; box-shadow: none;"> 5</label>
+                    <label style="margin-bottom: 0px;"><input type="checkbox" style="width: auto; box-shadow: none;"> 6 и более</label>
+                </div>
 
             </div>
 
         </div>
     </div>
+
 
 
     <div class="filter-right">
@@ -75,13 +73,13 @@
             </div>
 
         </div>
-        <button class="apply-filter">
-            Применить
-        </button>
+
     </div>
-
-
+    <button class="apply-filter">
+        Применить
+    </button>
 </section>
+
 
 <script>
     const filters = document.querySelectorAll(".filter-btn")
@@ -95,17 +93,27 @@
             document.querySelectorAll(".filter-dropdown").forEach(menu => {
 
                 if (menu !== dropdown) {
-                    menu.style.display = "none"
+                    menu.classList.remove("active")
                 }
 
             })
 
-            dropdown.style.display =
-                dropdown.style.display === "block" ?
-                "none" :
-                "block"
+            dropdown.classList.toggle("active")
 
         })
 
     })
+</script>
+
+<script>
+    const btn_mobile = document.querySelector('.filter__mobile');
+    const content1 = document.querySelector('.filter-left');
+    const content2 = document.querySelector('.filter-right');
+    const apply_btn = document.querySelector('.apply-filter');
+
+    btn_mobile.addEventListener('click', () => {
+        content1.classList.toggle('active');
+        content2.classList.toggle('active');
+        apply_btn.classList.toggle('active');
+    });
 </script>
